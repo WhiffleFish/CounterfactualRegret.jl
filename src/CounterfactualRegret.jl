aeac88b90ab892ffca6c40f787d53def2c9cf440
WhiffleFish <tylertronics@gmail.com>
2022-08-10 13:34:53 -0400

    updated file structure

--- src/CounterfactualRegret.jl
+++ src/CounterfactualRegret.jl
@@ -4,32 +4,32 @@ using ProgressMeter
 using RecipesBase
 
 
-include(joinpath("Extensive", "ExtensiveGames.jl"))
+include("ExtensiveGames.jl")
 export Game, initialhist, isterminal, utility, player, next_hist, infokey, actions
 export observation
 export chance_action, chance_actions, other_player
 export infokeytype, histtype
 export vectorized_hist, vectorized_info
 
-include(joinpath("Extensive", "baselines.jl"))
+include(joinpath("solvers", "baselines.jl"))
 export ZeroBaseline, ExpectedValueBaseline
 
-include(joinpath("Extensive", "CFR.jl"))
-include(joinpath("Extensive", "CSMCCFR.jl"))
-include(joinpath("Extensive", "ESMCCFR.jl"))
-include(joinpath("Extensive", "OSMCCFR.jl"))
+include(joinpath("solvers", "CFR.jl"))
+include(joinpath("solvers", "CSMCCFR.jl"))
+include(joinpath("solvers", "ESMCCFR.jl"))
+include(joinpath("solvers", "OSMCCFR.jl"))
 export CFRSolver, CSCFRSolver, ESCFRSolver, OSCFRSolver
 export train!, strategy
 
 
-include(joinpath("Extensive", "evaluation.jl"))
+include(joinpath("evaluation", "evaluation.jl"))
 export evaluate
 
 
-include(joinpath("Extensive", "exploitability.jl"))
+include(joinpath("evaluation", "exploitability.jl"))
 export ExploitabilitySolver, exploitability
 
-include(joinpath("Extensive", "callback.jl"))
+include(joinpath("evaluation", "callback.jl"))
 
 # Miscellaneous Games
 include(joinpath("games", "Games.jl"))
